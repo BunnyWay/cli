e020c265b53e0b79f587f21543a2cfd324721282
burstx86 <burstw0w@protonmail.com>
2026-06-03 00:26:07 +0200

    fix: removed unused subcommands

--- packages/cli/src/commands/pz/index.ts
+++ packages/cli/src/commands/pz/index.ts
@@ -1,4 +1,3 @@
-import type { CommandModule } from "yargs";
 import { defineNamespace } from "../../core/define-namespace.ts";
 import { pzCloneCommand } from "./clone.ts";
 import { pzCreateCommand } from "./create.ts";
@@ -9,90 +8,9 @@ import { pzPurgeCommand } from "./purge.ts";
 import { pzSelectCommand } from "./select.ts";
 import { pzShowCommand } from "./show.ts";
 
-
-
-const rulesList: CommandModule = {
-  command: "list <id>",
-  describe: "List edge rules for a pull zone.",
-  handler: () => {},
-};
-
-const rulesAdd: CommandModule = {
-  command: "add <id> <file>",
-  describe: "Add or update an edge rule from a JSON file.",
-  handler: () => {},
-};
-
-const rulesExport: CommandModule = {
-  command: "export <id> <name> [file]",
-  describe: "Export an edge rule by name to JSON file or stdout.",
-  handler: () => {},
-};
-
-const rulesCopy: CommandModule = {
-  command: "copy <source> <target>",
-  describe: "Copy all edge rules from one pull zone to another.",
-  handler: () => {},
-};
-
-const rulesDelete: CommandModule = {
-  command: "delete <id> <guid>",
-  describe: "Delete an edge rule by GUID.",
-  handler: () => {},
-};
-
-const rulesToggle: CommandModule = {
-  command: "toggle <id> <guid> <state>",
-  describe: "Enable or disable an edge rule.",
-  handler: () => {},
-};
-
-const hostnamesList: CommandModule = {
-  command: "list <id>",
-  describe: "List hostnames for a pull zone.",
-  handler: () => {},
-};
-
-const hostnamesAdd: CommandModule = {
-  command: "add <id> <hostname>",
-  describe: "Add a hostname to a pull zone.",
-  handler: () => {},
-};
-
-const hostnamesRemove: CommandModule = {
-  command: "remove <id> <hostname>",
-  describe: "Remove a hostname from a pull zone.",
-  handler: () => {},
-};
-
-const hostnamesCert: CommandModule = {
-  command: "cert <id> <hostname>",
-  describe: "Provision a Let's Encrypt SSL certificate for a hostname.",
-  handler: () => {},
-};
-
-const hostnamesForceSsl: CommandModule = {
-  command: "forcessl <id> <hostname> <state>",
-  describe: "Enable or disable Force SSL for a hostname.",
-  handler: () => {},
-};
-
-const rulesNamespace = defineNamespace("rules", "Manage pull zone edge rules.", [
-  rulesList,
-  rulesAdd,
-  rulesExport,
-  rulesCopy,
-  rulesDelete,
-  rulesToggle,
-]);
-
-const hostnamesNamespace = defineNamespace("hostnames", "Manage pull zone hostnames.", [
-  hostnamesList,
-  hostnamesAdd,
-  hostnamesRemove,
-  hostnamesCert,
-  hostnamesForceSsl,
-]);
+// TODO: implement rules and hostnames subcommands
+// const rulesNamespace = defineNamespace("rules", "Manage pull zone edge rules.", [...]);
+// const hostnamesNamespace = defineNamespace("hostnames", "Manage pull zone hostnames.", [...]);
 
 export const pzNamespace = defineNamespace(
   "pz",
@@ -106,7 +24,5 @@ export const pzNamespace = defineNamespace(
     pzPurgeCommand,
     pzShowCommand,
     pzDeselectCommand,
-    rulesNamespace,
-    hostnamesNamespace,
   ],
 );
